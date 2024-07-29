3f2a569dd0ebf022abe411ddb69b39baa0752816
eyalyehiely <eyalwork0@gmail.com>
2024-07-29 18:44:54 +0300

    clearify App.jsx to be more readable

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
-
-import './css/style.css';
-
-import './charts/ChartjsConfig';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 // Import pages
-import codeBlockPage from './components/codeBlockPage'
-import LobbyPage from './components/LobbyPage'
-// import Stocks from './components/Stocks';
-
-
-
+import CodeBlockPage from './components/CodeBlockPage';
+import LobbyPage from './components/LobbyPage';
 
 function App() {
-
-  const location = useLocation();
-
-  useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
-  }, [location.pathname]); // triggered on route change
-
   return (
-    <>
-    
-      <Routes>
-        
-        <Route exact path="/" element={<LobbyPage />} />
-        <Route path="/codeblock/:id" element={<CodeBlockPage />} />
-
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<LobbyPage />} />
+      <Route path="/codeblock/:id" element={<CodeBlockPage />} />
+    </Routes>
   );
-  
 }
 
 export default App;
