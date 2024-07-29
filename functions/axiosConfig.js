ccbadfdbd9e0bf45298382f874e57539090cb5b1
eyalyehiely <eyalwork0@gmail.com>
2024-07-29 16:59:08 +0300

    writing codeBlockPage component

--- functions/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_frontend_API_BASE_URL || 'http://localhost:8000/api/',
-  // other axios options if needed
-});
-
-export default instance;
