6e5afb2db0833520f543cd03762580de719fc2bb
eyalyehiely <eyalwork0@gmail.com>
2024-07-30 16:23:35 +0300

    add edit code func

---
+++ src/functions/codeBlocks/editCodeBlock.js
@@ -0,0 +1,15 @@
+import axios from '../axiosConfig';
+
+export default function editCodeBlock(id, code) {
+    axios.put(`/codeblock/${id}/edit/`, { code })
+    .then(response => {
+        if (response.status === 200 && response.data.success) {
+            console.log('Code block updated successfully');
+        } else {
+            console.error('Error: Unexpected response status', response.status);
+        }
+    })
+    .catch(error => {
+        console.error('Error updating code block:', error);
+    });
+}
