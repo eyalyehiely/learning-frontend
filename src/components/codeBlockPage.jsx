af1901dadb2db68ad205b1bfc4147bd5ba2e8c83
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 18:06:46 +0300

    implement submissions funcs in codeBlockPage

--- src/components/codeBlockPage.jsx
+++ src/components/codeBlockPage.jsx
@@ -1,27 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Typography, Paper } from '@mui/material';
 import Button from 'react-bootstrap/Button';
 import { useParams} from 'react-router-dom';
 import Editor from '@monaco-editor/react';
 import createSocket from '../functions/config/socketConfig';
-import fetchCurrentCodeBlock from '../functions/codeBlocks/fetchCurrentCodeBlock';
+import fetchCurrentSubmission from '../functions/submissions/fetchCurrentSubmission';
 import checkCode from '../functions/codeBlocks/checkCode';
-import editCodeBlock from '../functions/codeBlocks/editCodeBlock';
+import editSubmissionCodeBlock from '../functions/submissions/editSubmissionCodeBlock';
 import swal from 'sweetalert';
 
 const CodeBlockPage = () => {
     const { id } = useParams();
-    const [codeBlock, setCodeBlock] = useState({});
-    const [code, setCode] = useState('');
+    const [submissionCodeBlock, setSubmissionCodeBlock] = useState({});
+    const [code, setSubmissionCode] = useState('');
     const [originalCode, setOriginalCode] = useState('');
     const [role, setRole] = useState('');
     const [socket, setSocket] = useState(null);
     const [isCorrect, setIsCorrect] = useState(false);
 
     useEffect(() => {
-        fetchCurrentCodeBlock(id, (block) => {
-            setCodeBlock(block);
-            setCode(block.code);
+        fetchCurrentSubmission(id, (block) => {
+            setSubmissionCodeBlock(block);
+            setSubmissionCode(block.code);
             setOriginalCode(block.code);
         });
     }, [id]);
@@ -33,7 +34,7 @@ const CodeBlockPage = () => {
         newSocket.onmessage = (event) => {
             const data = JSON.parse(event.data);
             if (data.type === 'code_update') {
-                setCode(data.code);
+                setSubmissionCode(data.code);
             } else if (data.type === 'role') {
                 setRole(data.role);
             }
@@ -56,13 +57,13 @@ const CodeBlockPage = () => {
     }, [isCorrect]);
 
     const handleCodeChange = (newValue) => {
-        setCode(newValue);
+        setSubmissionCode(newValue);
         if (socket && socket.readyState === WebSocket.OPEN) {
             socket.send(JSON.stringify({ type: 'codeChange', code: newValue }));
         }
 
         // Send code update to backend
-        editCodeBlock(id, newValue);
+        editSubmissionCodeBlock(id, newValue);
     };
 
     
@@ -75,35 +76,36 @@ const CodeBlockPage = () => {
     };
 
     const handleResetCode = () => {
-        setCode(originalCode);
+        setSubmissionCode(originalCode);
     };
 
     return (
+        <>
+
         <Container style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '0 16px' }}>
-            <Button className='btn btn-primary' href='/'>
-                Back
-            </Button>
+            
             <Typography variant="h4" gutterBottom style={{ textAlign: 'center', marginTop: '16px' }}>
-                {codeBlock.id}. {codeBlock.title}
-                <Typography variant="subtitle1">{codeBlock.instructions}</Typography>
+                {submissionCodeBlock.id}. {submissionCodeBlock.title}
+                <Typography variant="subtitle1">{submissionCodeBlock.instructions}</Typography>
             </Typography>
-            <Paper style={{ flexGrow: 1, marginTop: '16px', padding: '16px', overflow: 'auto', width: '100%', maxWidth: '800px' }}>
+            <Paper style={{ height: '80vh',width:'60vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginLeft: '20%'}}>
                 <Editor
-                    height="50vh"
+                    height="60vh"
                     defaultLanguage="python"
                     value={code}
                     onChange={handleCodeChange}
-                    options={{ theme: 'vs-dark', readOnly: role === 'mentor' }}
+                    options={{ theme: 'vs-dark' }}
                 />
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '16px' }}>
                     <Button className="btn btn-primary" onClick={handleCheckCode}>Submit</Button>
+                    <Button variant="btn btn-secondary"><Link className="font-medium text-indigo-500 hover:text-indigo-600 dark:hover:text-indigo-400 no-underline" to="/">Back</Link></Button>
                     <Button className="btn btn-secondary" onClick={handleResetCode}>Reset</Button>
                 </div>
                 <div style={{ fontSize: '3em', textAlign: 'center', marginTop: '16px' }}>
-                    {/* {isCorrect ? '😊' : '😭'} */}
                 </div>
             </Paper>
         </Container>
+    </>
     );
 };
 
