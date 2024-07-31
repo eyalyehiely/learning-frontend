68fc1e52b286b031b173e0769b592645a4b25443
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 02:34:53 +0300

    add clientUUID handler

--- src/functions/fetchClientUuidtoServer.js
+++ src/functions/fetchClientUuidtoServer.js
@@ -2,19 +2,22 @@ import axios from '../functions/config/axiosConfig';
 import Cookies from 'js-cookie';
 
 export default function fetchClientUuidtoServer() {
-    const clientUUID = Cookies.get('clientUUID');  
+    const clientUUID = Cookies.get('clientUUID'); 
 
-    axios.post('/fetchClientUuidtoServer/', {
-        clientUUID,
-    })
-    .then(response => {
-        if (response.status === 200 || response.status === 201) {
-            console.log('Client UUID and code block ID sent to server'); // Ensure this matches the response structure
-        } else {
-            console.error('Error: Unexpected response status', response.status);
-        }
-    })
-    .catch(error => {
-        console.error('Error sending client UUID:', error);
-    });
+    if (!clientUUID) {
+        console.error('Error: clientUUID is not defined.');
+        return;
+    }
+
+    axios.post('/fetchClientUuidtoServer/', { clientUUID })
+        .then(response => {
+            if (response.status === 200 || response.status === 201) {
+                console.log(`Client UUID sent to server successfully: ${clientUUID}`);
+            } else {
+                console.error('Error: Unexpected response status', response.status);
+            }
+        })
+        .catch(error => {
+            console.error('Error sending client UUID:', error);
+        });
 }
