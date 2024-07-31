ee47f0b58810bc6d9fb2df5da928cd54891d4041
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 01:24:13 +0300

    fix fetchCurrentSubmission func

--- src/functions/submissions/fetchCurrentSubmission.js
+++ src/functions/submissions/fetchCurrentSubmission.js
@@ -1,11 +1,13 @@
 import axios from '../config/axiosConfig';
+import Cookies from 'js-cookie';
 
 export default function fetchCurrentCodeBlockSubmission(id, setSubmissionCodeBlock, setSubmissionCode) {
-    axios.get(`/codeblock/submission/${id}/`)
+    const clientUUID = Cookies.get('clientUUID'); 
+    axios.get(`/codeblock/submission/${id}/?user_id=${clientUUID}`)
         .then(response => {
-            if (response.status === 200) {
+            if (response.status === 200 || response.status === 201) {
                 setSubmissionCodeBlock(response.data);
-                setSubmissionCode(response.data.code);
+                setSubmissionCode(response.data.code); // Ensure this matches the response structure
             } else {
                 console.error('Error: Unexpected response status', response.status);
             }
