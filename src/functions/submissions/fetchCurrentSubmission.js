5e4e4db1c539a959b4e354ea44b7abd0921b86c7
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 02:24:47 +0300

    add uuid handle

--- src/functions/submissions/fetchCurrentSubmission.js
+++ src/functions/submissions/fetchCurrentSubmission.js
@@ -1,8 +1,15 @@
 import axios from '../config/axiosConfig';
 import Cookies from 'js-cookie';
 
-export default function fetchCurrentCodeBlockSubmission(id, setSubmissionCodeBlock, setSubmissionCode) {
+export default function fetchCurrentSubmission(id, setSubmissionCodeBlock, setSubmissionCode) {
     const clientUUID = Cookies.get('clientUUID'); 
+    console.log('Retrieved clientUUID:', clientUUID);
+
+    if (!clientUUID) {
+        console.error('Error: clientUUID is not defined.');
+        return;
+    }
+
     axios.get(`/codeblock/submission/${id}/?user_id=${clientUUID}`)
         .then(response => {
             if (response.status === 200 || response.status === 201) {
