a10ab60e0e4c90bf7fa15fa560061f76984cdde9
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 12:54:13 +0300

    fixing component route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 // Import pages
-import CodeBlockPage from './components/CodeBlockPage';
+import CodeBlockPage from './components/CodeBlockPage'
 import LobbyPage from './components/LobbyPage';
 
 function App() {
