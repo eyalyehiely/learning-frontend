7bdd289631c2dd8e46c15ffd2082ec01f4e6a909
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 01:22:15 +0300

    change codeblock route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import CodeBlockPage from './components/codeBlockPage';
+import CodeBlockPage from './components/CodeBlockPage';
 import LobbyPage from './components/LobbyPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LobbyPage />} />
-      <Route path="/codeblock/:id" element={<CodeBlockPage />} />
+      <Route path="/codeblock/:id/" element={<CodeBlockPage />} />
     </Routes>
   );
 }
