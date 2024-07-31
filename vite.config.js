1ef1f9cae1f5562965df78f503f4c0cafcf98a90
eyalyehiely <eyalwork0@gmail.com>
2024-07-31 15:36:39 +0300

    configure vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env
   },
-  
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: /^~.+/,
-        replacement: (val) => {
-          return val.replace(/^~/, "");
-        },
-      },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '^~(.+)': path.resolve(__dirname, '$1')
+    },
   },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
     }
-  } 
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true
+  }
 })
