d2f69818f5efcfe0908b7ec1fa77b842bae5f317
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 02:34:23 +0300

    change codeBlock page - add barrier to student and teacher

--- src/components/codeBlockPage.jsx
+++ src/components/codeBlockPage.jsx
@@ -2,30 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Typography, Paper } from '@mui/material';
 import Button from 'react-bootstrap/Button';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Editor from '@monaco-editor/react';
 import createSocket from '../functions/config/socketConfig';
-import fetchCurrentSubmission from '../functions/submissions/fetchCurrentSubmission';
 import checkCode from '../functions/codeBlocks/checkCode';
 import editSubmissionCodeBlock from '../functions/submissions/editSubmissionCodeBlock';
 import swal from 'sweetalert';
+import fetchSubmissionToCreate from '../functions/submissions/fetchSubmissionToCreate';
+import Cookies from 'js-cookie';
+import logVisitor from '../functions/logVisitor';
+import fetchCurrentSubmission from '../functions/submissions/fetchCurrentSubmission';
 
 const CodeBlockPage = () => {
     const { id } = useParams();
     const [submissionCodeBlock, setSubmissionCodeBlock] = useState({});
-    const [code, setSubmissionCode] = useState('');
+    const [submissionCode, setSubmissionCode] = useState('');
     const [originalCode, setOriginalCode] = useState('');
     const [role, setRole] = useState('');
     const [socket, setSocket] = useState(null);
     const [isCorrect, setIsCorrect] = useState(false);
+    const [isCodeChanging, setIsCodeChanging] = useState(false);
+
 
     useEffect(() => {
-        fetchCurrentSubmission(id, (block) => {
-            setSubmissionCodeBlock(block);
-            setSubmissionCode(block.code);
-            setOriginalCode(block.code);
-        });
+        const userId = Cookies.get('clientUUID');
+        fetchSubmissionToCreate(id, userId);
+        fetchCurrentSubmission(id, setSubmissionCodeBlock, setSubmissionCode);
     }, [id]);
+
+    useEffect(() => {
+        const url = window.location.href;  
+        logVisitor(url);
+    }, []);
 
     useEffect(() => {
         const newSocket = createSocket(`/ws/codeblock/${id}/`);
@@ -37,6 +45,9 @@ const CodeBlockPage = () => {
                 setSubmissionCode(data.code);
             } else if (data.type === 'role') {
                 setRole(data.role);
+            } else if (data.type === 'code_changing') {
+                setIsCodeChanging(true);
+                setTimeout(() => setIsCodeChanging(false), 1000); // Reset the flag after 1 second
             }
         };
 
@@ -56,23 +67,26 @@ const CodeBlockPage = () => {
         }
     }, [isCorrect]);
 
-    const handleCodeChange = (newValue) => {
-        setSubmissionCode(newValue);
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            socket.send(JSON.stringify({ type: 'codeChange', code: newValue }));
-        }
+    useEffect(() => {
+        const userRole = Cookies.get('role');
+        setRole(userRole);
+    }, []);
 
-        // Send code update to backend
-        editSubmissionCodeBlock(id, newValue);
+    const handleCodeChange = (newValue) => {
+        if (role === 'student') {
+            setSubmissionCode(newValue);
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                socket.send(JSON.stringify({ type: 'codeChange', code: newValue }));
+                socket.send(JSON.stringify({ type: 'code_changing' }));
+            }
+
+            // Send code update to backend
+            editSubmissionCodeBlock(id, newValue);
+        }
     };
 
-    
-
-
-
-
     const handleCheckCode = () => {
-        checkCode(id, code, setIsCorrect);
+        checkCode(id, submissionCode, setIsCorrect);
     };
 
     const handleResetCode = () => {
@@ -80,32 +94,38 @@ const CodeBlockPage = () => {
     };
 
     return (
-        <>
-
         <Container style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '0 16px' }}>
-            
             <Typography variant="h4" gutterBottom style={{ textAlign: 'center', marginTop: '16px' }}>
                 {submissionCodeBlock.id}. {submissionCodeBlock.title}
                 <Typography variant="subtitle1">{submissionCodeBlock.instructions}</Typography>
             </Typography>
-            <Paper style={{ height: '80vh',width:'60vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginLeft: '20%'}}>
+            <Paper style={{ height: '80vh', width: '60vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginLeft: '20%' }}>
                 <Editor
                     height="60vh"
                     defaultLanguage="python"
-                    value={code}
+                    value={submissionCode}
                     onChange={handleCodeChange}
-                    options={{ theme: 'vs-dark' }}
+                    options={{ theme: 'vs-dark', readOnly: role !== 'student' }}
                 />
+                {role === 'teacher' && isCodeChanging && (
+                    <Typography variant="subtitle1" color="secondary" style={{ marginTop: '16px' }}>
+                        Code is changing...
+                    </Typography>
+                )}
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '16px' }}>
-                    <Button className="btn btn-primary" onClick={handleCheckCode}>Submit</Button>
-                    <Button variant="btn btn-secondary"><Link className="font-medium text-indigo-500 hover:text-indigo-600 dark:hover:text-indigo-400 no-underline" to="/">Back</Link></Button>
-                    <Button className="btn btn-secondary" onClick={handleResetCode}>Reset</Button>
+                    {role === 'student' && (
+                        <>
+                            <Button className="btn btn-primary" onClick={handleCheckCode}>Submit</Button>
+                            <Button className="btn btn-secondary" onClick={handleResetCode}>Reset</Button>
+                        </>
+                    )}
+                    <Button variant="btn btn-secondary">
+                        <Link className="font-medium text-indigo-500 hover:text-indigo-600 dark:hover:text-indigo-400 no-underline" to="/">Back</Link>
+                    </Button>
                 </div>
-                <div style={{ fontSize: '3em', textAlign: 'center', marginTop: '16px' }}>
-                </div>
+                <div style={{ fontSize: '3em', textAlign: 'center', marginTop: '16px' }}></div>
             </Paper>
         </Container>
-    </>
     );
 };
 
