ec1c351e3b169b7decf7e52e528a28b018c615d2
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 21:44:09 +0300

    fixing socket route

--- src/functions/config/socketConfig.js
+++ src/functions/config/socketConfig.js
@@ -1,10 +1,8 @@
 const baseURL = import.meta.env.VITE_WEBSOCKET_BASE_URL || 'ws://localhost:8000/ws/';
 
 const createSocket = (path) => {
-    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-    const socketURL = baseURL.replace(/^ws:/, protocol).replace(/^wss:/, protocol);
 
-    const socket = new WebSocket(`${socketURL}${path}`);
+    const socket = new WebSocket(`${baseURL}${path}`);
 
     socket.onopen = () => {
         console.log('Connected to WebSocket server');
