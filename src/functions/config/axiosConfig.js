f5e8d20cb66142fc2100cfc65aa60b357ab96723
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 09:42:27 +0300

    change axios anme

--- src/functions/config/axiosConfig.js
+++ src/functions/config/axiosConfig.js
@@ -1,10 +1,17 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
+const instance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL || 'https://learning-backend-dev.up.railway.app/api/',
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
-export default axiosInstance;
+export default instance;
+
+
+
+
+
+
+
