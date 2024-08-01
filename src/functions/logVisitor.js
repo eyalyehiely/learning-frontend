c5dbb6d0bcbdb232dc04f280173feb4d9b9a27fa
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 19:38:44 +0300

    add cookie role statement in logvisitor

--- src/functions/logVisitor.js
+++ src/functions/logVisitor.js
@@ -4,6 +4,11 @@ import Cookies from 'js-cookie';
 export default function logVisitor(url) {
     const clientUUID = Cookies.get('clientUUID');  
 
+    if (!clientUUID) {
+        console.error('Error: clientUUID is not defined.');
+        return;
+    }
+
     axios.post('/log_visitor/', {
         clientUUID,
         url
@@ -12,8 +17,7 @@ export default function logVisitor(url) {
         if (response.status === 200) {
             const role = response.data.role;
             Cookies.set('role', role, { expires: 365 });
-            const clientRole = Cookies.get('role')
-            console.log(`Client UUID: ${clientUUID} is assigned the role of ${clientRole}`);
+            console.log(`Client UUID: ${clientUUID} is assigned the role of ${role}`);
         } else {
             console.error('Error: Unexpected response status', response.status);
         }
