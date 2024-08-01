93da7cc185261db5232457f5e242ead6bc37686e
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 06:31:02 +0300

    update base url

--- src/functions/config/axiosConfig.js
+++ src/functions/config/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_FRONTEND_API_BASE_URL || 'http://localhost:8000/api/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'https://learning-backend-dev.up.railway.app/api/',
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
-export default instance;
+export default axiosInstance;
