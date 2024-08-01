b080dcbbf730fcc89df2a8f8e74ac4ec4bd4719b
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 19:38:13 +0300

    fix fetch clien uuid

--- src/functions/fetchClientUuidtoServer.js
+++ src/functions/fetchClientUuidtoServer.js
@@ -1,14 +1,5 @@
 import axios from '../functions/config/axiosConfig';
-import Cookies from 'js-cookie';
-
-export default function fetchClientUuidtoServer() {
-    const clientUUID = Cookies.get('clientUUID'); 
-
-    if (!clientUUID) {
-        console.error('Error: clientUUID is not defined.');
-        return;
-    }
-
+export default function fetchClientUuidtoServer(clientUUID) {
     axios.post('/fetchClientUuidtoServer/', { clientUUID })
         .then(response => {
             if (response.status === 200 || response.status === 201) {
