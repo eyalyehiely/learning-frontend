5f26bda8076ece89e457eccdc1cad4395a82def3
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 06:56:11 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   },
   plugins: [react()],
   resolve: {
@@ -17,11 +17,21 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
+    },
+    minify: 'esbuild', // Enable minification
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
     }
   },
   server: {
     host: true,
     port: 5173,
-    strictPort: true
+    strictPort: true,
   }
-})
+});
