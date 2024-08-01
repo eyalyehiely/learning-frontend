af0a559a3ded2aefe9e759a66f792fcc1e7affff
eyalyehiely <eyalwork0@gmail.com>
2024-08-01 09:04:46 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env': process.env
   },
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '^~(.+)': path.resolve(__dirname, '$1')
     },
   },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    },
-    minify: 'esbuild', // Enable minification
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
     }
   },
   server: {
-    host: true,
+    host: '0.0.0.0',  // Ensure the server is accessible from outside the container
     port: 5173,
-    strictPort: true,
+    strictPort: true
   }
-});
+})
