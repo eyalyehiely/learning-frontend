111568a90c6faa54ff5caa72e6d6c7e87cb66cf6
eyalyehiely <eyalwork0@gmail.com>
2024-08-02 00:24:10 +0300

    fix socket config

--- src/functions/config/socketConfig.js
+++ src/functions/config/socketConfig.js
@@ -1,5 +1,4 @@
-const baseURL = import.meta.env.VITE_WEBSOCKET_BASE_URL || 'ws://localhost:8000/ws/';
-
+const baseURL = import.meta.env.VITE_WEBSOCKET_BASE_URL || 'ws://localhost:8000';
 const createSocket = (path) => {
     const socket = new WebSocket(`${baseURL}${path}`);
 
