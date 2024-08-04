bc267f29901cc6c1e6869c385aaa34873faab933
eyalyehiely <eyalwork0@gmail.com>
2024-08-04 14:09:24 +0300

    update vit config js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,43 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   define: {
+//     'process.env.NODE_ENV': JSON.stringify('production')
+//   },
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src')
+//     }
+//   },
+//   build: {
+//     minify: 'terser',
+//     terserOptions: {
+//       compress: {
+//         drop_console: true,
+//         dead_code: true
+//       }
+//     },
+//     commonjsOptions: {
+//       transformMixedEsModules: true
+//     }
+//   },
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173,
+//     strictPort: true
+//   }
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   define: {
-    'process.env': {
-      NODE_ENV: JSON.stringify('production')
-    }
+    'process.env.NODE_ENV': JSON.stringify('production')
   },
   plugins: [react()],
   resolve: {
@@ -15,6 +46,13 @@ export default defineConfig({
     }
   },
   build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        dead_code: true
+      }
+    },
     commonjsOptions: {
       transformMixedEsModules: true
     }
@@ -25,3 +63,4 @@ export default defineConfig({
     strictPort: true
   }
 });
+
