a6ccb83bc1e3f5b8538f1328a3f27f6d39231b33
eyalyehiely <eyalwork0@gmail.com>
2024-08-04 13:36:21 +0300

    update socket config

--- src/functions/config/socketConfig.js
+++ src/functions/config/socketConfig.js
@@ -1,25 +1,3 @@
-// const baseURL = import.meta.env.VITE_WEBSOCKET_BASE_URL || 'ws://localhost:8000';
-// const createSocket = (path) => {
-//     const socket = new WebSocket(`${baseURL}${path}`);
-
-//     socket.onopen = () => {
-//         console.log('Connected to WebSocket server');
-//     };
-
-//     socket.onclose = () => {
-//         console.log('Disconnected from WebSocket server');
-//     };
-
-//     socket.onerror = (error) => {
-//         console.error('WebSocket error', error);
-//     };
-
-//     return socket;
-// };
-
-// export default createSocket;
-
-
 const baseURL = import.meta.env.VITE_WEBSOCKET_BASE_URL || 'ws://localhost:8000';
 
 const createSocket = (path) => {
