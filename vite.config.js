16900cd4f02b1d38034291844698548c3e514914
eyalyehiely <eyalwork0@gmail.com>
2024-08-04 14:49:46 +0300

    update docker file

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-
-// export default defineConfig({
-//   define: {
-//     'process.env.NODE_ENV': JSON.stringify('production')
-//   },
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src')
-//     }
-//   },
-//   build: {
-//     minify: 'terser',
-//     terserOptions: {
-//       compress: {
-//         drop_console: true,
-//         dead_code: true
-//       }
-//     },
-//     commonjsOptions: {
-//       transformMixedEsModules: true
-//     }
-//   },
-//   server: {
-//     host: '0.0.0.0',
-//     port: 5173,
-//     strictPort: true
-//   }
-// });
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
